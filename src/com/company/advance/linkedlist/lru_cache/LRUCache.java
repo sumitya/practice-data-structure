0a8e2f9f6a4443b7f8320189ab65d9c84034823b
Sumit <syadavgit@gmail.com>
2022-08-27 12:33:54 +0530

    LRUCache working

--- src/com/company/advance/linkedlist/lru_cache/LRUCache.java
+++ src/com/company/advance/linkedlist/lru_cache/LRUCache.java
@@ -1,61 +1,89 @@
 package com.company.advance.linkedlist.lru_cache;
 
-import com.company.advance.linkedlist.DoubleLLNode;
-import com.company.advance.linkedlist.LLCreation;
 import java.util.HashMap;
+import java.util.Map;
 
 public class LRUCache {
-    final static int SIZE = 3;
-    public static void main(String[] args) {
-        int[] arr = new int[]{1, 2, 3, 2};
-        HashMap<Integer, DoubleLLNode> hm = new HashMap<>();
-        DoubleLLNode doubleLL = LLCreation.createDoubleLL(new int[]{1,2,3,4,5});
-        LLCreation.traverseDoubleLL(doubleLL);
-
-        for(int i = 0; i < arr.length; i++) {
-            if(hm.containsKey(arr[i])) {
-                DoubleLLNode dnode = hm.get(arr[i]);
-                if(dnode.next != null) {
-                    removeNode(doubleLL,dnode);
-                    insertAtBack(doubleLL,dnode);
-                }
-            } else {
-                DoubleLLNode dnode = new DoubleLLNode(arr[i]);
-                hm.put(arr[i],dnode);
-                if(hm.size() == SIZE) {
-                    removeNode(doubleLL,dnode);
-                    insertAtBack(doubleLL,dnode);
-                }
-            }
+    static class DLLNode{
+        int key;
+        int value;
+        DLLNode next;
+        DLLNode prev;
+        DLLNode(int key, int value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+            this.prev = null;
         }
     }
-    public static DoubleLLNode removeNode(DoubleLLNode doubleLL,DoubleLLNode dnode) {
-        DoubleLLNode temp = doubleLL;
-        DoubleLLNode nextNode = dnode.next;
-        DoubleLLNode prevNode = dnode.prev;
-
-        prevNode.next = nextNode;
-        nextNode.prev = prevNode;
-
-        return null;
+    private DLLNode dummy;
+    private DLLNode head;
+    private DLLNode tail;
+    private int capacity;
+    private Map<Integer, DLLNode> hm;
+    public LRUCache(int capacity) {
+        this.dummy = new DLLNode(-1,-1);
+        this.head = dummy;
+        this.tail = dummy;
+        this.capacity = capacity;
+        this.hm = new HashMap<>();
     }
-    public static DoubleLLNode insertAtBack(DoubleLLNode doubleLL,DoubleLLNode dnode) {
-        DoubleLLNode temp = doubleLL;
-        DoubleLLNode nextNode = dnode.next;
-        DoubleLLNode prevNode = dnode.prev;
-
-        prevNode.next = nextNode;
-        nextNode.prev = prevNode;
-
-        return temp;
-    }
-
     public int get(int key) {
-
-        return 1;
+        if(hm.containsKey(key)) {
+            DLLNode dnode = hm.get(key);
+            int val = dnode.value;
+            if(dnode !=tail) {
+                removeNode(dnode);
+                insertAtBack(dnode);
+            }
+            return val;
+        }
+        return -1;
     }
-
     public void set(int key, int value) {
+        if(hm.containsKey(key)) {
+            DLLNode dnode = hm.get(key);
+            dnode.value = value;
+            if(dnode != tail) {
+                removeNode(dnode);
+                insertAtBack(dnode);
+            }
+        }
+        else {
+            DLLNode dnode = new DLLNode(key,value);
+            if(hm.size() == capacity) {
+                hm.remove(head.next.key);
+                removeNode(head.next);
+            }
+            hm.put(key,dnode);
+            insertAtBack(dnode);
+        }
+    }
+    public void removeNode(DLLNode dnode) {
+        dnode.prev.next = dnode.next;
+        if(dnode == tail || dnode.next == null) {
+            tail = dnode.prev;
+        }else {
+            dnode.next.prev = dnode.prev;
+        }
+    }
+    public void insertAtBack(DLLNode dnode) {
+        tail.next = dnode;
+        dnode.prev = tail;
+        tail = dnode;
+    }
+    public static void main(String[] args) {
+        LRUCache lruCache = new LRUCache(2);
+        lruCache.set(1,10);
+        lruCache.set(2,20);
+        System.out.println(lruCache.get(2));
+        lruCache.set(2,20);
+        lruCache.set(2,20);
+        System.out.println(lruCache.capacity);
+        lruCache.set(3,30);
+        lruCache.set(4,40);
+        System.out.println(lruCache.get(3));
+        System.out.println(lruCache.capacity);
 
     }
 }
